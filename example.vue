83281c893bc3f2d288eb6afca925c907a89f366b
leafOfTree <node_github@163.com>
2020-01-07 11:00:04 +0800

    Add test with vue plugin configs enabled

--- example.vue
+++ example.vue
@@ -1,7 +1,11 @@
+<template lang="pug">
+  div.set
+    span(id="123")
+</template>
+
 <template>
   <p>{{ greeting }}</p>
 </template>
-
 
 <script>
 import MyComponent from '../components/MyComponent.vue';
@@ -15,9 +19,96 @@ export default {
 };
 </script>
 
+<script lang="ts">
+import Vue from 'vue'
+import { User } from '@/types'
+
+export default Vue.extend({
+  name: 'Home' as string,
+  data() {
+    return {
+      user: {} as User
+    }
+  },
+  computed: {
+    fullName(): string { // Computed Property returns a string
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  mounted(): void { // The mounted hook returns nothing
+    ...
+  }
+})
+</script>
+
+<script lang="coffee">
+# Assignment:
+number   = 42
+opposite = true
+
+# Conditions:
+number = -42 if opposite
+
+# Functions:
+square = (x) -> x * x
+
+# Arrays:
+list = [1, 2, 3, 4, 5]
+
+`
+function hello() {}
+console.log
+a = 123;
+`
+# Objects:
+math =
+  root:   Math.sqrt
+square: square
+cube:   (x) -> x * square x
+
+# Splats:
+race = (winner, runners...) ->
+print winner, runners
+
+# Existence:
+alert "I knew it!" if elvis?
+
+# Array comprehensions:
+cubes = (math.cube num for num in list)
+
+</script>
+
 <style scoped>
 p {
   font-size: 2em;
   text-align: center;
+}
+</style>
+
+<style scoped lang="scss">
+$a: 100px;
+
+div {
+  background: white;
+}
+</style>
+
+<style lang="sass">
+$w: 128px;
+
+.bg
+  $w: 128px;
+    background: white;
+  align-items: center;
+
+canvas
+  width: 100vw;
+</style>
+
+<style lang="less">
+@list: 10 12;
+canvas {
+  background: white;
+  width: 100vh;
 }
 </style>
--- lib/vimrc_template.vim
+++ lib/vimrc_template.vim
@@ -3,3 +3,13 @@ syntax on
 set rtp+=../%plugin
 set nocp
 set expandtab
+
+" vim-vue-plugin config
+let g:vim_vue_plugin_use_pug = 1
+let g:vim_vue_plugin_use_less = 1
+let g:vim_vue_plugin_use_sass = 1
+let g:vim_vue_plugin_use_coffee = 1
+let g:vim_vue_plugin_use_typescript = 1
+let g:vim_vue_plugin_load_full_syntax = 1
+let g:vim_vue_plugin_highlight_vue_attr = 1
+let g:vim_vue_plugin_use_foldexpr = 1
