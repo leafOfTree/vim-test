77e474d4d6f6ea2f0493f53ed57efd079b314b59
leafOfTree <node_github@163.com>
2020-01-07 11:17:21 +0800

    Support svelte with configs enabled

--- example.svelte
+++ example.svelte
@@ -2,13 +2,85 @@
   <p>{greeting}</p>
 </div>
 
+<template lang="pug">
+// Highlight everything in here as Pug.
+main
+  h1 
+    Balance
++await('getBalances')
+  Spinner
++then
+  Leader
+  History
+</template>
+
 <script>
   let greeting = 'Hello';
+</script>
+
+<script lang="coffee">
+  # Assignment:
+  number = 42
+  opposite = true
+
+  # Conditions:
+  number = -42 if opposite
+
+  # Functions:
+  square = (x) -> x * x
+
+  # Arrays:
+  list = [1, 2, 3, 4, 5]
+
+  # Objects:
+  math =
+    root:   Math.sqrt
+  square: square
+  cube:   (x) -> x * square x
+
+  # Splats:
+  race = (winner, runners...) ->
+  print winner, runners
+
+  # Existence:
+  alert "I knew it!" if elvis?
+
+  # Array comprehensions:
+  cubes = (math.cube num for num in list)
+
 </script>
 
 <style>
   p {
     font-size: 2em;
     text-align: center;
+  }
+</style>
+
+<style scoped lang="scss">
+  $a: 100px;
+
+  div {
+    background: white;
+  }
+</style>
+
+<style lang="sass">
+  $w: 128px;
+
+  .bg
+  $w: 128px;
+    background: white;
+  align-items: center;
+
+  canvas
+    width: 100vw;
+</style>
+
+<style lang="less">
+  @list: 10 12;
+  canvas {
+    background: white;
+    width: 100vh;
   }
 </style>
--- lib/vimrc_template.vim
+++ lib/vimrc_template.vim
@@ -14,3 +14,12 @@ let g:vim_vue_plugin_use_typescript = 1
 let g:vim_vue_plugin_load_full_syntax = 1
 let g:vim_vue_plugin_highlight_vue_attr = 1
 let g:vim_vue_plugin_use_foldexpr = 1
+
+" vim-svelte-plugin config
+let g:vim_svelte_plugin_use_pug = 1
+let g:vim_svelte_plugin_use_less = 1
+let g:vim_svelte_plugin_use_sass = 1
+let g:vim_svelte_plugin_use_coffee = 1
+let g:vim_svelte_plugin_load_full_syntax = 1
+let g:vim_svelte_plugin_highlight_svelte_attr = 1
+let g:vim_svelte_plugin_use_foldexpr = 1
