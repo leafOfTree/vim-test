94455a2595629ef10972f973afcd696f5173b1b2
leafOfTree <node_github@163.com>
2021-03-11 16:55:41 +0800

    Update

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ then
   exit
 fi
 
+vim --version | head -3
+
 filetype=$1 # "svelte"
 plugin=$2 # "vim-svelte-plugin"
 
@@ -25,7 +27,6 @@ plugin_dir="../$plugin"
 
 echo "[test] Current path is $parent_path"
 echo "[test] Plugin is $plugin_dir for $filetype"
-vim --version
 
 cd "$parent_path"
 if [ ! -d "$plugin_dir" ] 
